5796cef665f7f654bf446aa801a41d8d8ac3a485
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-11-05 07:26:21 +0000

    feat: integrate Clerk authentication and add button component

---
+++ src/components/layout/LayoutClient.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { ClerkProvider } from "@clerk/nextjs";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function LayoutClient({
+    children
+} : {
+    children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <header>
+        <SignedOut>
+        <SignInButton />
+        <SignUpButton>
+          <Button>Sign up</Button>
+        </SignUpButton>
+      </SignedOut>
+
+      <SignedIn>
+        <Link href={"/"}>Dashboard</Link>
+      </SignedIn>
+      </header>
+
+      {children}
+    </ClerkProvider>
+  );
+}
